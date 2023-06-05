9d5e293539d4de97b56c934df2d46ea64b5633a1
SheldonYoungo <sheldomyoungo@gmail.com>
2023-06-05 17:18:17 -0400

    Fixed bugs with the deleting function

--- script.js
+++ script.js
@@ -1,51 +1,19 @@
-
 import fs from "fs";
-// inquirer
-// // THIS WILL BE THE MESSAGE DISPLAYED WHEN INITIALIZING THE PROGRAM, THE ANSWER WILL BE STORED IN A PROPERTY CALLED "URL"
-//   .prompt([
-//     {"message" : "Type in your URL: ", "name" : "URL"}
-//   ])
-
-//   // HANDLING THE ANSWER
-//   .then((answers) => {
-
-//     const url = answers.URL; //Storing the answer inside a constant
-  
-//     try {
-//       let qr_code = qr.image(url); //Generating the image data
-//       let domain = new URL(url).hostname; //Converting the url into an URL object to get its domain
-//       domain = domain.substring(domain.lastIndexOf(".", domain.lastIndexOf(".") - 1) + 1); //Trimming the domain to get only its name
-
-//       qr_code.pipe(fs.createWriteStream(`qr_codes/${domain}_qr_code.png`)); //Finally writing the image data inside a .png file with the name of the domain
-      
-//       // Creating a text file to store the given input
-//       fs.writeFile(`qr_codes/${domain}_input.text`, url, (err) => {
-//           if(err) throw err;
-
-//           console.log("The file has been succesfully saved!!")
-//       })
-    
-//     // HANDLING ERRORS
-//     } catch(err) {
-//       console.log("Invalid URL");
-//     } 
-//   })
 
 function deletePreviousQR() {
-    const urlExtensions = ['.com', '.net', '.io', '.gob', '.org', '.us', '.co'];
-    let currentQR = fs.readdirSync('public/qr_codes');
+    const urlExtentions = ['.com', '.net', '.io', '.gob', '.org', '.us', '.co']; // Parameter to check if the QR name match one of this common domain extentions
+    let currentQR = fs.readdirSync('public/qr_codes'); // Looks inside the qr_codes directory and returns an array with the files inside it.
+    let filesToDelete; 
 
-    let filesToDelete = [];
-
-    urlExtensions.forEach(extension => {
+    urlExtentions.forEach(extention => {
         currentQR.forEach(qr => {
-            if(qr.includes(extension)) {
-               filesToDelete.push(qr);
+            if(qr.includes(extention)) {
+               filesToDelete = qr;
             }
         })
     })
 
-    filesToDelete.forEach(file => fs.unlinkSync(`public/qr_codes/${file}`))
+    fs.unlinkSync(`public/qr_codes/${filesToDelete}`)
 }
 
 export {deletePreviousQR};
