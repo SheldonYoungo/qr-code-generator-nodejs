301f099ee791ca93700d5d4170e533f267ca032c
SheldonYoungo <sheldomyoungo@gmail.com>
2023-08-22 15:33:31 -0400

    fix(script.js) Fixed semantic issue

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-function deletePreviousQR() {
+export function deletePreviousQR() {
     const urlExtentions = ['.com', '.net', '.io', '.gob', '.org', '.us', '.co']; // Parameter to check if the QR name match one of this common domain extentions
     let currentQR = fs.readdirSync('public/qr_codes'); // Looks inside the qr_codes directory and returns an array with the files inside it.
     let filesToDelete; 
@@ -15,8 +15,3 @@ function deletePreviousQR() {
 
     fs.unlinkSync(`public/qr_codes/${filesToDelete}`)
 }
-
-export {deletePreviousQR};
-
-
-
